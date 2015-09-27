c974bf5a4ed540810954ce03776f16b06161a92a
abdulbasitgulsen <abdulbasitgulsen@192.168.1.26>
2015-09-27 22:06:33 +0300

    changes

--- samples/Pages/WebContent/pages/page1.controller.js
+++ samples/Pages/WebContent/pages/page1.controller.js
@@ -2,9 +2,9 @@ sap.ui.define([
    "sap/ui/core/mvc/Controller"
 ], function (Controller) {
    "use strict";
-   return Controller.extend("mvcgiris.main", {
-		onPress : function() {	
-			alert("Merhaba");		
+   return Controller.extend("pages.page1", {
+		onSayfa2 : function() {
+			app.to("idpage22");
 		}
    });
 });
--- samples/Pages/WebContent/pages/page2.controller.js
+++ samples/Pages/WebContent/pages/page2.controller.js
@@ -2,9 +2,9 @@ sap.ui.define([
    "sap/ui/core/mvc/Controller"
 ], function (Controller) {
    "use strict";
-   return Controller.extend("mvcgiris.main", {
-		onPress : function() {	
-			alert("Merhaba");		
+   return Controller.extend("pages.page2", {
+		onGeri : function() {
+			app.back();
 		}
    });
 });
